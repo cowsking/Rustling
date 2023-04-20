71111b31d09ff99180e5edd4924859c97e4cf23f
CowsKing <CowsKing@github.com>
2023-04-20 18:44:42 -0400

    save

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,10 +2,12 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
+    
+
+    let word:&str = &word;
     if is_a_color_word(word) {
         println!("That is a color word I know!");
     } else {
